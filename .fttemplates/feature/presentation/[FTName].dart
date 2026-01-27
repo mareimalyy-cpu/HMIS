0fa337eb13a7c389c5806bf2a3193fc10a40d9de
Mostafa Abdulrhman <mostafashridam@gmail.com>
2026-01-27 18:10:19 +0200

    Add templates folder as normal files

---
+++ .fttemplates/feature/presentation/[FTName].dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class [FTName | pascalcase]Page extends ConsumerWidget {
+  const [FTName | pascalcase]Page({super.key});
+
+  static const routeName = '/[FTName | snakecase]';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
